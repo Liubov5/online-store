c734b6a8950be0db894aa029a5884eb0cca9b747
Liubov5 <liubov.iakovleva5@gmail.com>
2023-08-22 19:46:59 +0300

    work with db

--- server/index.js
+++ server/index.js
@@ -1,9 +1,19 @@
-require('dotenv').config({path: '.env'});
+require('dotenv').config();
 
 const express = require('express');
+const sequelize = require('./db')
 const port = process.env.PORT;
-
 const app = express();
 
-app.listen(port, ()=> console.log(port)); 
+const start = async()=>{
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        app.listen(port, ()=> console.log(port)); 
+    }catch(e){
+        console.log(e)
+    }
+}
+
+start();
 
