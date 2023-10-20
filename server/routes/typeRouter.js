4a766396f7eb038e1b5d99576e70e0bcb7983d19
Liubov5 <liubov.iakovleva5@gmail.com>
2023-10-20 23:08:53 +0300

    started frontend

--- server/routes/typeRouter.js
+++ server/routes/typeRouter.js
@@ -1,10 +1,10 @@
 const Router = require('express');
 const router = new Router();
 const typeController = require("../controllers/typeController");
+const checkRoleMiddleware = require("../middleware/checkRoleMiddleware");
 
 
-
-router.post("/", typeController.create); //добавлять бренды
+router.post("/", checkRoleMiddleware("ADMIN"), typeController.create); //добавлять бренды
 router.get("/", typeController.getAll) //получать бренды
 
 
--- server/routes/userRouter.js
+++ server/routes/userRouter.js
@@ -1,11 +1,11 @@
 const Router = require('express');
 const router = new Router();
-const UserController = require("../controllers/userController");
 const userController = require('../controllers/userController');
+const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/registration", userController.registration); //регистрация
 router.post("/login", userController.login); 
-router.get('/auth', userController.check); //авторизован пользователь или нет по jwt токену
+router.get("/auth", authMiddleware, userController.check); //авторизован пользователь или нет по jwt токену
 
 
 module.exports = router;
