b138d2e636ffbed141d605526340913eff5299be
Liubov5 <liubov.iakovleva5@gmail.com>
2024-08-21 23:12:38 +0300

    add devicepage

--- client/src/components/DeviceItem.js
+++ client/src/components/DeviceItem.js
@@ -2,28 +2,34 @@ import React from 'react'
 import Col from 'react-bootstrap/Col';
 import Card from 'react-bootstrap/Card';
 import Image from 'react-bootstrap/Image';
-import star from "../images/star.png"
+import star from "../images/star.png";
+import { Link, useNavigate } from "react-router-dom";
+import { DEVICE_ROUTE } from '../utils/consts';
+
 const DeviceItem = ({id,name,price, img, rating}) => {
-    
-  return (
-    <Col md='3' className='mt-3'>
-        <Card style={{width:150, cursor:'pointer'}} border={"dark"}>
-            <Image width={150} height={150} src={img}/>
-            <div className='text-black-50 mt-2 d-flex justify-content-between align-items-center'>
-                <div>Samsung...</div>
-                <div className='d-flex align-items-center'>
-                    <div>{rating}</div>
-                    <Image width={18} height={18} src={star}/>
-                </div>
-            </div>
-            <div className='mt-2 d-flex justify-content-between align-items-center'>
-                <div>{name}</div>
-                <div>{price}</div>
-            </div>
-            
-        </Card>
-    </Col>
-  )
+    const navigate = useNavigate();
+    console.log(navigate)
+    return (
+        <Col md='3' className='mt-3'>
+            <Link to={`${DEVICE_ROUTE}/${id}`}>
+                <Card style={{width:150, cursor:'pointer'}} border={"dark"}>
+                    <Image width={150} height={150} src={img}/>
+                    <div className='text-black-50 mt-2 d-flex justify-content-between align-items-center'>
+                        <div>Samsung...</div>
+                        <div className='d-flex align-items-center'>
+                            <div>{rating}</div>
+                            <Image width={18} height={18} src={star}/>
+                        </div>
+                    </div>
+                    <div className='mt-2 d-flex justify-content-between align-items-center'>
+                        <div>{name}</div>
+                        <div>{price}</div>
+                    </div>
+                    
+                </Card>
+            </Link>
+        </Col>
+    )
 }
 
 export default DeviceItem
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,8 +1,43 @@
 import React from 'react'
+import Container from 'react-bootstrap/esm/Container'
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
+import BigStar from "../images/BigStar.png";
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 
 const DevicePage = () => {
+  const device =  {
+      id:6,
+      name:"Iphone 11",
+      price: "600",
+      img:"https://economictimes.indiatimes.com/thumb/msid-95136602,width-1200,height-900,resizemode-4,imgsize-18404/iphone-15-pro.jpg?from=mdr",
+      rating:5
+    }
+
+    const description = {
+      //вот тут остановилась
+    }
   return (
-    <h1>DevicePage</h1>
+    <Container className='mt-5 d-flex'>
+        <Col md={4}>
+          <Image width={300} height={300} src={device.img}/>
+        </Col>
+        <Col md={4}>
+          <div className='d-flex flex-column align-items-center'>
+              <h2>{device.name}</h2>
+              <div style={{background:`url(${BigStar}) no-repeat center center`, width:240, height:240, backgroundSize:'cover', fontSize:64}} className='d-flex align-items-center justify-content-center'>
+                {device.rating}
+              </div>
+          </div>
+        </Col>
+        <Col md={4}>
+          <Card style={{width:300, height:300, fontSize:32, border:'2px solid lightgray'}} className='d-flex flex-column align-items-center justify-content-around'>
+              <h3>От: {device.price} $</h3>
+              <Button variant='outline-dark'>Добавить в корзину</Button>
+          </Card>
+        </Col>
+    </Container>
   )
 }
 
